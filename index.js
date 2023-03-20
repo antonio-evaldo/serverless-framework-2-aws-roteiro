8a6cbea520df633bd14807f4c8a651af1a71518d
Antônio Evaldo <antonio.evaldo349@gmail.com>
2023-03-20 17:14:51 -0300

    video-2.2

--- index.js
+++ index.js
@@ -1,13 +1,22 @@
-module.exports.cadastrarAlunos = async (event) => {
-  return {
-    statusCode: 200,
-    body: JSON.stringify(
-      {
-        message: "Go Serverless v3.0! Your function executed successfully!",
-        input: event,
-      },
-      null,
-      2
-    ),
-  };
+module.exports.simulandoUploadDoCSV = async (evento) => {
+  try {
+    console.log("Simule aqui o upload do arquivo...");
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify({
+        mensagem: "Simulando upload de arquivo..."
+      })
+    }
+  } catch (erro) {
+    return {
+      statusCode: erro.statusCode || 500,
+      body: JSON.stringify(erro)
+    }
+  }
+}
+
+module.exports.cadastrarAlunos = async (evento) => {
+  console.log("Função 'cadastrarAlunos' foi executada por um evento do S3! Informações do evento: ");
+  console.log(evento);
 };
