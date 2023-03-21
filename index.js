7c0f4f93253fc50e9ec484ea0f4b26e4f2702f4b
Antônio Evaldo <antonio.evaldo349@gmail.com>
2023-03-21 18:45:31 -0300

    video-2.3

--- index.js
+++ index.js
@@ -1,6 +1,27 @@
+const { S3Client, PutObjectCommand } = require("@aws-sdk/client-s3");
+
+async function fazUploadNoBucket() {
+  const cliente = new S3Client({
+    forcePathStyle: true,
+    credentials: {
+      accessKeyId: "S3RVER",
+      secretAccessKey: "S3RVER"
+    },
+    endpoint: "http://localhost:4569"
+  });
+
+  const comandoUpload = new PutObjectCommand({
+    Bucket: "alunos-csv-local",
+    Key: "cadastrar_alunos.csv",
+    Body: Buffer.from("1234")
+  })
+
+  await cliente.send(comandoUpload);
+}
+
 module.exports.simulandoUploadDoCSV = async (evento) => {
   try {
-    console.log("Simule aqui o upload do arquivo...");
+    await fazUploadNoBucket();
 
     return {
       statusCode: 200,
